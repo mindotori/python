2d9ad98c770112ae1b5fd21582013f69ff977c41
김민주 <tptlffldk97@naver.com>
2025-04-06 02:56:44 +0900

    code up 6009

---
+++ string_1.py
@@ -0,0 +1,14 @@
+# 거꾸로 뒤집기
+문자열 = "Hello World!"
+print(문자열[::-1])
+
+
+
+# 중복 제거 (set)
+temp = [1,1,2,2,3,4,4,5]
+print(list(set(temp)))
+
+
+print("\"!@#$%^&*()\'")
+print("\"C:\Download\\'hello\'.py\"")
+print("print(\"Hello\\nWorld\")")

62160e078b00987b46c5ccd0450efa99de8c74aa
김민주 <alswn971@nate.com>
2025-04-06 13:48:44 +0900

    codeup 6022~6025

---
+++ coding test/codeUp100/6022_6025.py
@@ -0,0 +1,18 @@
+# 6022 
+# s[a:b] -> s의 a번째 인덱스 부터 b-1번째 인덱스까지 잘라냄
+#s = input()
+#print(s[0:2],s[2:4],s[4:6])
+
+# 6023
+#시,분,초 = input().split(':')
+#print(분)
+
+# 6024
+#w1, w2 = input().split()
+#s = w1+w2
+#print(s)
+
+# 6025
+a,b = input().split()
+c = int(a) + int(b)
+print(c)

740321bcae7ad41e9696598ff2713bcc03287c09
김민주 <alswn971@nate.com>
2025-04-06 14:21:51 +0900

    codeup 6026-6030

---
+++ coding test/codeUp100/6026_6030.py
@@ -0,0 +1,31 @@
+# 6026
+# a = input()
+# b = input()
+# c = float(a) + float(b)
+# print(c)
+
+# 6027
+# 정수를 16진수로 출력 (소문자))
+#a = input()
+#n = int(a)
+#print('%x' %n)
+
+# 6028
+# 10진수를 입력받아 16진수로 출력 (대문자)
+# a = input()
+# n = int(a)
+# print('%X' %n) 
+
+# 6029
+#a = input()
+#n = int(a, 16) # 입력된 a를 16진수로 인식해 변수 n에 저장
+#print('%o' %n) # 16진수를 입력받아 8진수로 출력
+
+# 6030
+n = ord(input())
+print(n)
+
+
+
+
+

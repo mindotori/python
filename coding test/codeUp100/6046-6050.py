0a18ab9431c3470a8edb77e6bd23169b491f94dd
김민주 <alswn971@nate.com>
2025-04-06 17:25:35 +0900

    codeup 6046-6050

---
+++ coding test/codeUp100/6046-6050.py
@@ -0,0 +1,26 @@
+# 6046
+# a = int(input())
+# print(a<<1)
+# print(a>>1)
+
+# 6047
+# a,b = input().split()
+# a = int(a)
+# b = int(b)
+# print(a<<b)
+
+# 6048
+# a,b = input().split()
+# a,b = int(a),int(b)
+# print(a<b)
+
+# 6049
+# a,b = input().split()
+# a,b = int(a), int(b)
+# print(a==b)
+    
+# 6050((
+a,b = input().split()
+a,b = int(a), int(b)
+print(b >= a)
+
